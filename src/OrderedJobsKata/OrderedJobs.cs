e0aed5dd5693ab9439bb784878333b5ffaabf31a
Martin Rue <martin@invalidcast.com>
2011-09-27 14:05:43 +0100

    step 1 - empty string

---
+++ src/OrderedJobsKata/OrderedJobs.cs
@@ -0,0 +1,9 @@
+﻿public class OrderedJobs
+{
+    public string Jobs { get; private set; }
+
+    public OrderedJobs(string jobStructure)
+    {
+        Jobs = string.Empty;
+    }
+}
---
+++ src/OrderedJobsKata/Specs.cs
@@ -0,0 +1,16 @@
+﻿using Machine.Specifications;
+
+public class when_job_structure_is_empty_string
+{
+    Because of = () =>
+    {
+        orderedJobs = new OrderedJobs(string.Empty);
+    };
+
+    It should_produce_an_empty_sequence = () =>
+    {
+        orderedJobs.Jobs.Length.ShouldEqual(0);
+    };
+
+    static OrderedJobs orderedJobs;
+}
